2925822ec6e5dec5a39d268f9782cfdd12ede655
Gerardo Tacay Terrazos <gerardo.tacay@ibm.com>
2023-10-16 15:56:49 -0500

    ultimo

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -3,7 +3,6 @@ import { Grid, Column } from '@carbon/react';
 import RepoTable from './RepoTable';
 import TablaSeveridad from './../../components/TablaSeveridad';
 import InfoQuarter from './../../components/InfoQuarter';
-import './_repo-page.scss';
 import { Row } from 'carbon-components-react';
 import OpenCases from './../../components/OpenCases'
 import './_repo-page.scss'
@@ -20,46 +19,27 @@ const headers1 = [
 
 const rows1 = [];
 
-// const RepoPage = () => {
-//   return (
-//     <Grid className="repo-page">
-//       <Column lg={16} className="repo-page__r1">
-//       <div className='container'>
-//     <TablaSeveridad/>    
-//     <InfoQuarter/>
-//     <InfoQuarter/>
-//     </div>
-//       </Column>
-//       <Column lg={16} className="repo-page__r1">
-//         <RepoTable headers={headers1} rows={rows1} />
-//       </Column>
-//     </Grid>
-//   );
-// };
 
 const RepoPage = () => {
   return (
-    <Grid className="repo-page">
-        <Column lg = {15}>
-        <OpenCases/>
-        </Column>
-            <Column lg={4}> 
-                <TablaSeveridad/>
-            </Column>
-            <Column lg ={12}> 
-                <Row> 
-                     <Column>
-                        <div id='tarjetas'>
-                            <InfoQuarter/>
-                            <InfoQuarter/>
-                        </div>
-                    </Column>
-                </Row>
-                <Row>
-                   <RepoTable/> 
-                </Row>
-            </Column>
+    <Grid id="repo-page">
+      <Column lg={16}>
+        <OpenCases />
+      </Column>
+      <Column lg={4}>
+        <TablaSeveridad />
+      </Column>
+      <Column lg={12}>
+        <div id='tarjetas'>
+          <InfoQuarter />
+          <InfoQuarter />
+        </div>
+      </Column>
+      <Column lg = {16}>
+        <RepoTable />
+      </Column>
     </Grid>
   );
 };
+
 export default RepoPage;
