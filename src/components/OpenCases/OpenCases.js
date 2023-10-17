edb7dd6c55c5d00adc913156816c6b494cf4c4b6
Gerardo Tacay Terrazos <gerardo.tacay@ibm.com>
2023-10-17 10:54:00 -0500

    Agregados dropdowns unnastable

--- src/components/OpenCases/OpenCases.js
+++ src/components/OpenCases/OpenCases.js
@@ -1,50 +1,73 @@
 import React from 'react';
 import { Dropdown } from '@carbon/react';
 import './_open-cases.scss';
+import { FluidForm } from 'carbon-components-react';
+import { unstable__FluidDropdown as FluidDropdown } from '@carbon/react';
+import { ToggletipLabel } from '@carbon/react';
+import { ToggletipButton } from '@carbon/react';
+import { Toggletip } from '@carbon/react';
+import { Information } from '@carbon/icons-react';
+import { ToggletipContent } from '@carbon/react';
 
 const OpenCases = () => (
   <div>
     <div className='box-container'>
-        <p> Open Cases</p>
-    <div className="open-cases-container">
-      <div id="dropdown-container">
-        <Dropdown
-          id="dropdown1"
-          titleText="Dropdown 1"
-          label="Dropdown Label 1"
+  <FluidDropdown
+    className="test-class"
+    id="default"
+    invalidText="Error message that is really long can wrap to more lines but should not be excessively long."
+    itemToString={item => item ? item.text: ''}
+    items={[
+      {
+        id: 'option-0',
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+        label: 'esto es un label'
+      },
+      {
+        id: 'option-1',
+        text: 'Option 1'
+      },
+      {
+        id: 'option-2',
+        text: 'Option 2'
+      },
+      {
+        disabled: true,
+        id: 'option-3',
+        text: 'Option 3 - a disabled item'
+      },
+      {
+        id: 'option-4',
+        text: 'Option 4'
+      },
+      {
+        id: 'option-5',
+        text: 'Option 5'
+      }
+    ]}
+    label="Choose an option"
+    playgroundWidth={400}
+    titleText="Label"
+    warnText="Warning message that is really long can wrap to more lines but should not be excessively long."
+  />
+  <br />
+  <FluidDropdown
+    className="test-class"
+    id="default-3"
+    invalidText="Error message that is really long can wrap to more lines but should not be excessively long."
+    itemToString={item => item ? item.text: ''}
           items={[
-            { id: 'option-1', text: 'Option 1' },
-            { id: 'option-2', text: 'Option 2' },
-          ]}
-        />
-      </div>
-
-      <div id="dropdown-container">
-        <Dropdown
-          id="dropdown2"
-          titleText="Dropdown 2"
-          label="Dropdown Label 2"
-          items={[
-            { id: 'option-3', text: 'Option 3' },
-            { id: 'option-4', text: 'Option 4' },
-          ]}
-        />
-      </div>
-
-      <div id="dropdown-container">
-        <Dropdown
-          id="dropdown3"
-          titleText="Dropdown 3"
-          label="Dropdown Label 3"
-          items={[
-            { id: 'option-5', text: 'Option 5' },
+            { id: 'option-5', text: 'Option 5' , label: 'texto' },
             { id: 'option-6', text: 'Option 6' },
           ]}
-        />
-      </div>
-    </div>
+    label="Choose an option"
+    playgroundWidth={400}
+    titleText={<><ToggletipLabel>Label</ToggletipLabel><Toggletip align="top-left"><ToggletipButton label="Show information"><Information /></ToggletipButton><ToggletipContent><p>Additional field information here.</p></ToggletipContent></Toggletip></>}
+    warnText="Warning message that is really long can wrap to more lines but should not be excessively long."
+  />
     </div>
   </div>
 );
+
 
 export default OpenCases;
