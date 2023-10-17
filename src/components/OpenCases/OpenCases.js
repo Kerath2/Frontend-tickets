6d86f86bcd07c3f9e53d50fe4182db3c5890c885
Gerardo Tacay Terrazos <gerardo.tacay@ibm.com>
2023-10-17 12:31:51 -0500

    Agregado titulo

--- src/components/OpenCases/OpenCases.js
+++ src/components/OpenCases/OpenCases.js
@@ -15,55 +15,56 @@ var fechaCorte = 'Viernes 18/08/2023';
 
 const OpenCases = () => (
     <div className='box-container'>
-      <div id = 'dropdownContainer'> 
-        <FluidDropdown
-          className="test-class"
-          id="default-3"
-          invalidText="Error message that is really long can wrap to more lines but should not be excessively long."
-          itemToString={item => item ? item.text: ''}
-                items={[
-                  { id: 'option-5', text: 'Option 5' , label: 'texto' },
-                  { id: 'option-6', text: 'Option 6' },
-                ]}
-          label= {fechaCorte}
-          playgroundWidth={400}
-          titleText={<><ToggletipLabel>Fecha de corte</ToggletipLabel><Toggletip align="top-left"><ToggletipButton label="Show information"><Information /></ToggletipButton><ToggletipContent><p>La fecha de corte se refiere a</p></ToggletipContent></Toggletip></>}
-          warnText="Warning message that is really long can wrap to more lines but should not be excessively long."
-        />
-      </div>
-      <div id = 'dropdownContainer'> 
-        <FluidMultiSelect
-          className="test-class"
-          id="default-3"
-          invalidText="Error message that is really long can wrap to more lines but should not be excessively long."
-          itemToString={item => item ? item.text: ''}
-                items={[
-                  { id: 'option-5', text: 'Option 5' , label: 'texto' },
-                  { id: 'option-6', text: 'Option 6' },
-                ]}
-          label= 'Mostrar todo'
-          playgroundWidth={400}
-          titleText={<><ToggletipLabel>Severidad</ToggletipLabel><Toggletip align="top-left"><ToggletipButton label="Show information"><Information /></ToggletipButton><ToggletipContent><p>La fecha de corte se refiere a</p></ToggletipContent></Toggletip></>}
-          warnText="Warning message that is really long can wrap to more lines but should not be excessively long."
-        />
-      </div>
-
-
-      <div id = 'dropdownContainer'> 
-        <FluidDropdown
-          className="test-class"
-          id="default-3"
-          invalidText="Error message that is really long can wrap to more lines but should not be excessively long."
-          itemToString={item => item ? item.text: ''}
-                items={[
-                  { id: 'option-5', text: 'Option 5' , label: 'texto' },
-                  { id: 'option-6', text: 'Option 6' },
-                ]}
-          label= 'Comparación'
-          playgroundWidth={400}
-          titleText={<><ToggletipLabel>Fecha de corte</ToggletipLabel><Toggletip align="top-left"><ToggletipButton label="Show information"><Information /></ToggletipButton><ToggletipContent><p>La fecha de corte se refiere a</p></ToggletipContent></Toggletip></>}
-          warnText="Warning message that is really long can wrap to more lines but should not be excessively long."
-        />
+      <h3> Open Cases </h3>
+      <div className='filaDropDowns'> 
+        <div id = 'dropdownContainer'> 
+          <FluidDropdown
+            className="test-class"
+            id="default-3"
+            invalidText="Error message that is really long can wrap to more lines but should not be excessively long."
+            itemToString={item => item ? item.text: ''}
+                  items={[
+                    { id: 'option-5', text: 'Option 5' , label: 'texto' },
+                    { id: 'option-6', text: 'Option 6' },
+                  ]}
+            label= {fechaCorte}
+            playgroundWidth={400}
+            titleText={<><ToggletipLabel>Fecha de corte</ToggletipLabel><Toggletip align="top-left"><ToggletipButton label="Show information"><Information /></ToggletipButton><ToggletipContent><p>La fecha de corte se refiere a</p></ToggletipContent></Toggletip></>}
+            warnText="Warning message that is really long can wrap to more lines but should not be excessively long."
+          />
+        </div>
+        <div id = 'dropdownContainer'> 
+          <FluidMultiSelect
+            className="test-class"
+            id="default-3"
+            invalidText="Error message that is really long can wrap to more lines but should not be excessively long."
+            itemToString={item => item ? item.text: ''}
+                  items={[
+                    { id: 'option-5', text: 'Option 5' , label: 'texto' },
+                    { id: 'option-6', text: 'Option 6' },
+                  ]}
+            label= 'Mostrar todo'
+            playgroundWidth={400}
+            titleText={<><ToggletipLabel>Severidad</ToggletipLabel><Toggletip align="top-left"><ToggletipButton label="Show information"><Information /></ToggletipButton><ToggletipContent><p>La fecha de corte se refiere a</p></ToggletipContent></Toggletip></>}
+            warnText="Warning message that is really long can wrap to more lines but should not be excessively long."
+          />
+        </div>
+        <div id = 'dropdownContainer'> 
+          <FluidDropdown
+            className="test-class"
+            id="default-3"
+            invalidText="Error message that is really long can wrap to more lines but should not be excessively long."
+            itemToString={item => item ? item.text: ''}
+                  items={[
+                    { id: 'option-5', text: 'Option 5' , label: 'texto' },
+                    { id: 'option-6', text: 'Option 6' },
+                  ]}
+            label= 'Comparación'
+            playgroundWidth={400}
+            titleText={<><ToggletipLabel>Fecha de corte</ToggletipLabel><Toggletip align="top-left"><ToggletipButton label="Show information"><Information /></ToggletipButton><ToggletipContent><p>La fecha de corte se refiere a</p></ToggletipContent></Toggletip></>}
+            warnText="Warning message that is really long can wrap to more lines but should not be excessively long."
+          />
+        </div>
       </div>
     </div>
 );
