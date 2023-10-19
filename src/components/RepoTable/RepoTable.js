c91628d61b98412976e7ba941e0a26a19202c0fb
Gerardo Tacay Terrazos <gerardo.tacay@ibm.com>
2023-10-18 22:28:03 -0500

    agregada descripcion tabla

--- src/components/RepoTable/RepoTable.js
+++ src/components/RepoTable/RepoTable.js
@@ -15,6 +15,13 @@ import {
 
 
 const RepoTable = ({ rows, headers }) => {
+
+  const getRowDescription = (rowId) => {
+    const row = rows.find(({ id }) => id === rowId);
+    return row ? row.description : '';
+  };
+
+
   return (
     <DataTable
       rows={rows}
@@ -50,7 +57,7 @@ const RepoTable = ({ rows, headers }) => {
                     ))}
                   </TableExpandRow>
                   <TableExpandedRow colSpan={headers.length + 1}>
-                    <p>Row description</p>
+                    <p>{getRowDescription(row.id)}</p>
                   </TableExpandedRow>
                 </React.Fragment>
               ))}
